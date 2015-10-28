23d1a40826b0e10d08d9110b4eec71e5b6348be2
Alex K. Fong <alexkfong@yahoo.com>
2015-10-29 01:56:37 +0800

    case handling for victory functioning, and function to update feedback message

--- js/app.js
+++ js/app.js
@@ -1,10 +1,31 @@
 
 $(document).ready(function(){
-	
-	var newGame = startGame();
 
+	// start a new game with a number and guesses	
+	var currentGame = startGame();
+
+	console.log("Page reloaded");
+	console.log("Secret number is " + currentGame.number);
+
+	// start a new game if new game in nav is clicked
 	$(".new").click(function(){
-		var newGame = startGame();		
+		var currentGame = startGame();		
+	});
+
+	//Check a guess
+	$("#guessButton").click( function(){
+		
+		var guess = $('#userGuess').filter(':input').val();
+
+		// Make sure the guess is valid
+		if( parseInt(guess, 10) && !currentGame.won ) {
+
+			giveFeedback(checkGuess(guess, currentGame));
+		}
+		else if ( currentGame.won ) {
+			giveFeedback("You won already!");
+		}
+		
 	});
 
 	/*--- Display information modal box ---*/
@@ -19,6 +40,7 @@ $(document).ready(function(){
 
 });
 
+// generates a random number
 function randomGameNumber (minimum, maximum) {
 
 	randomNumber = Math.floor( Math.random() * (maximum - minimum + 1)) + minimum; 
@@ -26,14 +48,75 @@ function randomGameNumber (minimum, maximum) {
 
 };
 
+// function creates a new game by returning an object
 function startGame () {
 
 	var number = randomGameNumber(1, 100);
-	var guesses = 0;
+	var totalGuesses = 0;
+	var won = false;
+	var lastGuess;
+	clearFeedback();
 
 	return {
 		number: number, 
-		guesses: guesses
+		totalGuesses: totalGuesses,
+		won: won.
+		lastGuess: lastGuess
 	};
 
 };
+
+// this function checks which is the larger number
+// and then submits to buildMessage for the actual check
+function checkGuess (guess, game) {
+
+	var message;
+	
+	// winning guess case
+	if( guess == game.number) {
+		message = "You win!";
+		game.won = true;
+	}
+	else if ( guess > game.number) {
+		message = buildMessage( guess, game.number);
+	}
+	else {
+		message = buildMessage( game.number, guess);
+	}
+
+	return message;
+}
+
+// Constructs the message based on difference between
+// the guess and the secret number
+function buildMessage (largerNumber, smallerNumber) {
+	
+	var message;
+
+	if( (largerNumber - smallerNumber) > 50 ) {
+			message = "Ice cold."
+	}
+	else if( (largerNumber - smallerNumber) > 30 ) {
+			message = "Cold."
+	}
+	else if( (largerNumber - smallerNumber) > 20 ) {
+		message = "Warm."
+	}
+	else if( (largerNumber - smallerNumber) > 10 ) {
+		message = "Hot."
+	}
+	else {
+		message = "Very hot."
+	}
+
+	return message;
+
+};
+
+function giveFeedback(message) {
+	$('#feedback').text(message);
+};
+
+function clearFeedback() {
+	$('#feedback').text("Makes your Guess!");
+}

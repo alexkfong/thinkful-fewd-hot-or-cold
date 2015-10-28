12b2fe4d7482f34f271e07576f0d144ee818ede4
Alex K. Fong <alexkfong@yahoo.com>
2015-10-29 00:42:03 +0800

    Created newGame function, and made new game button functional.

--- js/app.js
+++ js/app.js
@@ -1,10 +1,15 @@
 
 $(document).ready(function(){
 	
+	var newGame = startGame();
+
+	$(".new").click(function(){
+		var newGame = startGame();		
+	});
+
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
-
   	});
 
   	/*--- Hide information modal box ---*/
@@ -14,4 +19,21 @@ $(document).ready(function(){
 
 });
 
+function randomGameNumber (minimum, maximum) {
 
+	randomNumber = Math.floor( Math.random() * (maximum - minimum + 1)) + minimum; 
+	return randomNumber;
+
+};
+
+function startGame () {
+
+	var number = randomGameNumber(1, 100);
+	var guesses = 0;
+
+	return {
+		number: number, 
+		guesses: guesses
+	};
+
+};
